c0cbeec9b65273e7966162a6882254d9c5769c65
Mariana Melo Pereira <144270249+marip864@users.noreply.github.com>
2025-07-08 09:22:05 -0300

    Arquivos iniciais - Aula 07/07

---
+++ decomposicao_LU.py
@@ -0,0 +1,37 @@
+matriz = []
+elementoInt = []
+vetorInt = []
+n = int(input('Digite a ordem (n) da matriz A:'))
+x = [0]*n
+for i in range(n):
+    elemento = input('Digite os coeficientes da linha:')
+    elemento = elemento.split()
+    for e in elemento:
+        e = int(e)
+        elementoInt.append(e)
+    matriz.append(elementoInt)
+    elementoInt = []
+print(matriz)
+
+
+# Inicialização de L e U
+L = [[0 for _ in range(n)] for _ in range(n)]
+U = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    # Matriz U
+    for k in range(i, n):
+        U[i][k] = matriz[i][k] - sum(L[i][j] * U[j][k] for j in range(i))
+
+    # Matriz L
+    L[i][i] = 1
+    for k in range(i+1, n):
+        L[k][i] = (matriz[k][i] - sum(L[k][j] * U[j][i] for j in range(i))) / U[i][i]
+
+print("Matriz L:")
+for linha in L:
+    print(linha)
+
+print("\nMatriz U:")
+for linha in U:
+    print(linha)
