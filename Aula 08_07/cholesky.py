bd7120238b3a1c60d2def6376e1814a1650dbd86
marip864 <cl202239@g.unicamp.br>
2025-07-10 08:52:02 -0300

    Atualização Cholesky

--- Aula 08_07/cholesky.py
+++ Aula 08_07/cholesky.py
@@ -1,3 +1,4 @@
+import math
 matriz = []
 elementoInt = []
 vetorInt = []
@@ -11,30 +12,17 @@ for i in range(n):
         elementoInt.append(e)
     matriz.append(elementoInt)
     elementoInt = []
-print(matriz)
 linha = [0]*n
-lower = []
-for a in range(n):
-    lower.append(linha)
-for i in range(n):
-    for j in range(i+1):
-        soma = 0
-        if i==j:
-            for k in range(j):
-                soma+= (lower[j][k]**2)
-            lower[j][j] = int((matriz[j][j]-soma)**(1/2))
-        else:
-            for k in range(j):
-                soma+= (lower[i][k]*lower[j][k])
-            if lower[j][j]>0:
-                lower[i][j]=(matriz[i][j]-soma)/lower[j][j]
-    
-    print('triangular inferior\t\ttransposta')
-    
-    for i in range(n):
-        for j in range(n):
-            print(lower[i][j], end='\t')
-        print()
-        for j in range(n):
-            print(lower[j][i], end='\t')
-        print()
+g = [[0]*n]*n
+for k in range(n):
+    s = 0
+    for j in range(k):
+        s = s + g[k][j]**2
+    r = matriz[k][k] - s
+    g[k][k] = math.sqrt(r)
+    for i in range(k+1, n):
+        s = 0
+        for j in range(1, k-1):
+            s = s + g[i][j]*g[k][j]
+        g[i][k] = (matriz[i][k] - s)/g[k][k]
+print(g,'\t')
